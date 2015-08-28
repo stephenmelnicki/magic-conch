975b026d5d8cef52d0ac51751a57eb847ec65a04
smelnicki <smelnicki3@gmail.com>
2015-08-28 15:27:33 -0500

    renaming files from conch to index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Path = require('path');
 
 module.exports = function (robot) {
-  var path = Path.resolve(__dirname, 'scripts', 'conch.js');
+  path = Path.resolve(__dirname, 'scripts');
   robot.load(path);
 };
